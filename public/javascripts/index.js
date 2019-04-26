d3ccd113cf37e58c338c09d3dd2cc340d76d1085
iqbal <asifmai@hotmail.com>
2019-04-26 23:31:43 +0500

    first commit

---
+++ public/javascripts/index.js
@@ -0,0 +1,24 @@
+$(function() {
+  var BV = new $.BigVideo({useFlashForFirefox:false});
+BV.init();
+if ((/iPhone|iPod|iPad|Android|BlackBerry/).test(navigator.userAgent)) {
+  BV.show('img/cover.png');
+} else {
+  BV.show('http://video-js.zencoder.com/oceans-clip.mp4',{
+    altSource:'http://video-js.zencoder.com/oceans-clip.ogv'
+  });
+}
+BV.getPlayer().volume(0);
+
+$('body').on('click', '.mute.muted', function(){
+BV.getPlayer().volume(0.8);
+$(this).removeClass('muted').addClass('unmuted');
+return false;
+});
+
+$('body').on('click', '.mute.unmuted', function(){
+BV.getPlayer().volume(0);
+$(this).removeClass('unmuted').addClass('muted');
+return false;
+});
+});

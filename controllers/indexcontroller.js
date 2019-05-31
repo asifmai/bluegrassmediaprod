36d0c4bc2c7b83d9dfef60b499b55f3b469043f7
iqbal <asifmai@hotmail.com>
2019-06-01 00:41:25 +0500

    'update'

--- controllers/indexcontroller.js
+++ controllers/indexcontroller.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 const StaticContent = require('../models/staticcontent');
+const Tag = require('../models/tag');
+const Project = require('../models/project');
 
 module.exports.index_get = async (req, res, next) => {
   const static = await StaticContent.find();
-  res.render('index', {staticcontent: static[0]});
+  const tags = await Tag.find();
+  const projects = await Project.find().populate('tags').exec();
+  res.render('index', {staticcontent: static[0], tags, projects});
 }
